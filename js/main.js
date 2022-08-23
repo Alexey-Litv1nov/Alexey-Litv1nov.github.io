00f6d8b18cf7ed6be1319869db5645111590021e
Alexey-Litv1nov <58080613+Alexey-Litv1nov@users.noreply.github.com>
2022-08-23 15:55:16 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// hamburger menu
+let hamburgerBtn = document.querySelector(".header__hamburger-btn");
+let hamburderContent = document.querySelector(".hamburger-content");
+function hamburger() {
+    hamburgerBtn.addEventListener("click",()=> {
+        hamburgerBtn.classList.toggle("btn-active");
+        hamburderContent.classList.toggle("hamburger-active");
+    });
+}
+// close hamburger menu without btn
+document.addEventListener("click",(e)=> {
+    const click = e.composedPath().includes(hamburderContent);
+    const btnClick = e.composedPath().includes(hamburgerBtn);
+    if(!click && !btnClick) {
+        hamburgerBtn.classList.remove("btn-active");
+        hamburderContent.classList.remove("hamburger-active");
+    }
+});
+hamburger();
+
+// tabs
