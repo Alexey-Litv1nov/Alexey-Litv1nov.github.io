b0ac7a3d45fd8c09550e6c7153ed8bd069ba6f11
Alexey-Litv1nov <58080613+Alexey-Litv1nov@users.noreply.github.com>
2022-08-31 22:40:17 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
-// hamburger menu
-let hamburgerBtn = document.querySelector(".header__hamburger-btn");
-let hamburderContent = document.querySelector(".hamburger-content");
-function hamburger() {
-    hamburgerBtn.addEventListener("click",()=> {
-        hamburgerBtn.classList.toggle("btn-active");
-        hamburderContent.classList.toggle("hamburger-active");
-    });
-}
-// close hamburger menu without btn
-document.addEventListener("click",(e)=> {
-    const click = e.composedPath().includes(hamburderContent);
-    const btnClick = e.composedPath().includes(hamburgerBtn);
-    if(!click && !btnClick) {
-        hamburgerBtn.classList.remove("btn-active");
-        hamburderContent.classList.remove("hamburger-active");
+window.addEventListener("DOMContentLoaded", () => {
+// hamburger menun
+    let hamburgerBtn = document.querySelector(".header__hamburger-btn");
+    let hamburderContent = document.querySelector(".hamburger-content");
+    function hamburger() {
+        hamburgerBtn.addEventListener("click",()=> {
+            hamburgerBtn.classList.toggle("btn-active");
+            hamburderContent.classList.toggle("hamburger-active");
+        });
     }
-});
-hamburger();
+    // close hamburger menu without btn
+    document.addEventListener("click",(e)=> {
+        const click = e.composedPath().includes(hamburderContent);
+        const btnClick = e.composedPath().includes(hamburgerBtn);
+        if(!click && !btnClick) {
+            hamburgerBtn.classList.remove("btn-active");
+            hamburderContent.classList.remove("hamburger-active");
+        }
+    });
+    hamburger();
 
-// tabs
+    // tabs
+});
